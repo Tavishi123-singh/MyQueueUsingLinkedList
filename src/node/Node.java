0172b008fa2ed703e8c2bb4ec3456555e0ec99bc
Tavishi Singh <tavishisinghx@gmail.com>
2020-04-29 01:33:55 +0530

    Final Commit

--- src/node/Node.java
+++ src/node/Node.java
@@ -5,4 +5,27 @@
 package node;
 
 public class Node {
+    private int data;
+    private Node next;
+
+    public Node(int data) {
+        this.data = data;
+        next=null;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
 }
