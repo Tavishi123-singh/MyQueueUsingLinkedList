7abb9eed0eaf00cca1f00837a99f30feae1c38e7
Tavishi Singh <tavishisinghx@gmail.com>
2020-04-29 01:57:07 +0530

    Final Commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,5 +4,19 @@
  */
 package main;
 
+import myqueue.MyQueueUsingLinkedList;
+
 public class Main {
+    public static void main(String[] args) {
+        MyQueueUsingLinkedList queue=new MyQueueUsingLinkedList();
+        queue.enqueue(1000);
+        System.out.println(queue.getSize());
+        System.out.println(queue.peek().getData() + " element at front");
+        queue.enqueue(2000);
+        queue.enqueue(3000);
+        queue.enqueue(4000);
+        System.out.println(queue.dequeue().getData() + " element deleted from queue");
+        System.out.println("The size queue is = " + queue.getSize());
+        System.out.println(queue.peek().getData());
+    }
 }
